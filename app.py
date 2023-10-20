e666feb6f2fd89ea78c4713cfb6b66c9aa7dd458
Rishabh Singh <56092654+rishabhzz@users.noreply.github.com>
2023-10-20 07:50:31 +0530

    Update and rename app.js to app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name)
+
+# Store messages in memory (for simplicity)
+messages = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    message = data['message']
+    messages.append(message)
+    return 'Message sent!', 200
+
+@app.route('/get_messages')
+def get_messages():
+    return {'messages': messages}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)  # Change host and port as needed
